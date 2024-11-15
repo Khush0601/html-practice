06f5d1ada490e10c7500f853ae0a5d44ebfc573b
khushboo kumari <ksingh200601@gmail.com>
2024-11-16 03:27:25 +0530

    flexbox learnt and revising javascript concepts of datatypes

---
+++ index.js
@@ -0,0 +1,16 @@
+// Primitive Datatypes:
+// string
+name="khush"
+console.log( typeof(name))
+
+//number
+num=-1.2
+console.log( typeof (num))
+
+console.log( typeof(100/0))  //Infinity but when typeof asked it will be number
+console.log(typeof (-100/0)) //-Infinity
+console.log(typeof (0/0))    //Nan
+
+//BigInt
+num2=1234567653578888999n;
+console.log(typeof(num2))
